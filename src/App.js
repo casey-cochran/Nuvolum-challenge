2125ae6ddddb5c479b5a4b38c8744d3bd314a2d9
Casey Cochran <Casey3362@yahoo.com>
2022-09-10 15:05:05 -0700

    created fetch call to retrieve products, data expected is being returned

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const getProducts = async() => {
+      const response = await fetch('https://fakestoreapi.com/products?limit=5');
+      const data = await response.json();
+      console.log(data)
+    }
+    getProducts();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello
     </div>
   );
 }
