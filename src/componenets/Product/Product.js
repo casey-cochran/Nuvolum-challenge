4412457d8c921299ed539b24d590d887c630313e
Casey Cochran <Casey3362@yahoo.com>
2022-09-10 15:12:11 -0700

    created product component, now rendering each product

---
+++ src/componenets/Product/Product.js
@@ -0,0 +1,14 @@
+
+
+const Product = ({product}) => {
+    console.log(product, 'in product')
+
+    return (
+        <div>
+            hello product
+        </div>
+    )
+}
+
+
+export default Product;
