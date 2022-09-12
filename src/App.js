b332b4d83d05e4811dcf56ec903ad3ce77079507
Casey Cochran <Casey3362@yahoo.com>
2022-09-11 18:52:27 -0700

    minor code changes, stlying, and seo improvements added

--- src/App.js
+++ src/App.js
@@ -5,27 +5,37 @@ import "./App.css";
 function App() {
   const [products, setProducts] = useState([]);
   const [currentProduct, setCurrentProduct] = useState(null);
+  const [errors, setErrors] = useState([]);
   const scrollRef = useRef(null);
-  const executeScroll = () => scrollRef.current.scrollIntoView()
+  const executeScroll = () => scrollRef.current.scrollIntoView();
 
   useEffect(() => {
+    //Fetch products on page load
     const getProducts = async () => {
-      const response = await fetch("https://fakestoreapi.com/products?limit=5");
-      const data = await response.json();
-      setProducts([...products, ...data]);
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products?limit=5"
+        );
+        const data = await response.json();
+        setProducts([...products, ...data]);
+      } catch (err) {
+        //Can set errors returned here in useState and display formatted error to users
+      }
     };
     getProducts();
-
   }, []);
 
   const openProduct = (product) => {
-    if(currentProduct && product.id === currentProduct.id){
+    //This function sets a current product so that it is viewable
+    //If there is already a current product, remove that product so that the container collapses/closes
+    if (currentProduct && product.id === currentProduct.id) {
       setCurrentProduct(null);
-    }else{
+    } else {
+      //scroll function for when an item is opened, you scroll into view of its details
       executeScroll();
       setCurrentProduct(product);
     }
-  }
+  };
 
   return (
     <div className="home-cont">
@@ -37,23 +47,30 @@ function App() {
           quis magnam?
         </p>
       </div>
+      {/* Could render errors here if there is no data returned from fetch call */}
       <div className="item-cont">
-        {products.length > 0 ?
-        <div className="flex-products">
-          {products?.map((product, idx) => {
-            return (
-              <div
-                className="product-cont"
-                key={product.id}
-                onClick={() => openProduct(product)}
-              >
-                <Product product={product} key={product.id} currentProduct={currentProduct?.id === product.id && currentProduct} />
-              </div>
-            );
-          })}
-        </div>
-        : <div className="loading">Please wait while your products load...</div> }
-        <div className={currentProduct ? 'product-desc' : 'product-desc hide'}>
+        {products.length > 0 ? (
+          <div className="flex-products">
+            {products?.map((product) => {
+              return (
+                <div
+                  className="product-cont"
+                  key={product.id}
+                  onClick={() => openProduct(product)}
+                >
+                  <Product
+                    product={product}
+                    key={product.id}
+                    currentProduct={currentProduct?.id === product.id}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loading">Please wait while your products load...</div>
+        )}
+        <div className={currentProduct ? "product-desc" : "product-desc hide"}>
           <div ref={scrollRef} className="desc-cont">
             <div className="product-info">
               <div className="price-rating">${currentProduct?.price}</div>
@@ -63,7 +80,13 @@ function App() {
               </div>
             </div>
             <div className="add-cart">
-            <button className="submit-btn" type="submit">Add to cart</button>
+              <button
+                onClick={() => alert("Not yet implemented")}
+                className="submit-btn"
+                type="submit"
+              >
+                Add to cart
+              </button>
             </div>
           </div>
         </div>
--- src/componenets/Product/Product.js
+++ src/componenets/Product/Product.js
@@ -1,24 +1,22 @@
-import './Product.css';
-import {FaChevronCircleLeft} from 'react-icons/fa';
+import "./Product.css";
+import { FaChevronCircleLeft } from "react-icons/fa";
 
-const Product = ({product, currentProduct}) => {
-
-    return (
-        <>
-            <div className='single-product'>
-                <div>
-                    <img src={product.image} alt="Image of product" />
-                </div>
-                <div className='product-title'>
-                    <h3 >{product.title}</h3>
-                </div>
-                <div>
-                <FaChevronCircleLeft className={currentProduct ? 'chevron down' : 'chevron'} />
-                </div>
-            </div>
-        </>
-    )
-}
-
+const Product = ({ product, currentProduct }) => {
+  return (
+      <div className="single-product">
+        <div>
+          <img src={product.image} alt={product.title} />
+        </div>
+        <div className="product-title">
+          <h3>{product.title}</h3>
+        </div>
+        <div>
+          <FaChevronCircleLeft
+            className={currentProduct ? "chevron down" : "chevron"}
+          />
+        </div>
+      </div>
+  );
+};
 
 export default Product;
